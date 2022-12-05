df6538ed7c32fdddf723ee2ab0799153334aff10
Jannik Kreucher <jannik.kreucher@th-bingen.de>
2022-12-05 11:29:50 +0100

    display, buttons and compare implemented

--- main.cpp
+++ main.cpp
@@ -17,6 +17,10 @@
 #define PIN_LED4    PA_5
 #define PIN_BUZZER  PB_3
 
+// user parameters (times 2.5ms)
+#define BUTTON_LONG     200
+#define BUTTON_SHORT    20
+
 
 /*** 7 segment display data ***/
 //  0bHGFEDCBA active low
@@ -49,14 +53,35 @@ const uint16_t segment_digit[4] = {
     0b00000001
 };
 
+// states
+enum eDisplayMode {
+    StateCompare,
+    StateSettings
+};
+
 
 /*** global vars ***/
 // main vars
-uint8_t bMainUpdate = 0;
+volatile uint8_t bMainUpdate = 0;
+
 // display vars
 uint8_t bDisplayDigit = 0;
 uint8_t bDisplayPoint = 4; // decimal point place (4 for none)
 uint8_t bDisplayValues[4] = {16, 16, 16, 16}; // display values for each digit
+uint8_t bDisplayState = StateCompare;
+uint16_t wDecimalCounter = 0;
+// adc stuff
+int16_t wAdcValue = 0;
+int16_t wAdcSetpoint = 0;
+uint8_t bAdcEnableCompare = 1;
+// buttons
+uint16_t bSW1_Timer = 0;
+uint16_t bSW2_Timer = 0;
+uint16_t bSW3_Timer = 0;
+uint8_t bButtonUpEnable = 0;
+uint16_t bButtonUpCounter = 0;
+uint8_t bButtonDownEnable = 0;
+uint16_t bButtonDownCounter = 0;
 
 
 Ticker tMainUpdate;
@@ -78,25 +103,30 @@ AnalogIn pinPot(A0);
 void vMainUpdate();
 void vShiftOut(uint16_t wData);
 void vDisplayUpdate();
+void vCheckButtons();
 
 
 // main() runs in its own thread in the OS
 int main() {
-    // main update interrupt
-    tMainUpdate.attach(&vMainUpdate, 2500us);
     // leds off
     busLeds = 0xF;
+    pinBuzzer = 1;
+    // default state
+    bDisplayState = StateSettings;
+    // main update interrupt
+    tMainUpdate.attach(&vMainUpdate, 2500us);
 
     while(1) {
         // recalculate
-        int32_t voltage = (float)(3.3 * pinPot) * 1000.0;
-        bDisplayValues[0] = (voltage       ) % 10;
-        bDisplayValues[1] = (voltage /   10) % 10;
-        bDisplayValues[2] = (voltage /  100) % 10;
-        bDisplayValues[3] = (voltage / 1000) % 10;
+        wAdcValue = (float)(3.3 * pinPot) * 1000.0;
+
+        // compare setpoint to actual value
+        if((wAdcValue > wAdcSetpoint) && (bAdcEnableCompare)) busLeds = 0;
+        else busLeds = 0xF;
 
         // update stuff
         vDisplayUpdate();
+        vCheckButtons();
 
         // wait for next update period
         while(bMainUpdate);
@@ -125,6 +155,39 @@ void vShiftOut(uint16_t wData) {
 
 
 void vDisplayUpdate() {
+    // state machine
+    switch(bDisplayState) {
+        case StateCompare:
+            // show if compre mode is enabled
+            if(bAdcEnableCompare) bDisplayValues[3] = 0xC;
+            else bDisplayValues[3] = 16;
+            // show analog voltage
+            bDisplayPoint = 2;
+            bDisplayValues[0] = (wAdcValue /   10) % 10;
+            bDisplayValues[1] = (wAdcValue /  100) % 10;
+            bDisplayValues[2] = (wAdcValue / 1000) % 10;
+            break;
+        
+        case StateSettings:
+            // blink decimal point
+            if(wDecimalCounter == 0) {
+                // reset timer
+                wDecimalCounter = 400;
+                // toggle decimal point
+                if(bDisplayPoint < 4) bDisplayPoint = 4;
+                else bDisplayPoint = 2;
+            } else {
+                // decrement timer
+                wDecimalCounter--;
+            }
+            // show set voltage
+            bDisplayValues[0] = (wAdcSetpoint /   10) % 10;
+            bDisplayValues[1] = (wAdcSetpoint /  100) % 10;
+            bDisplayValues[2] = (wAdcSetpoint / 1000) % 10;
+            bDisplayValues[3] = 16;
+            break;
+    }
+
     if(bDisplayPoint == bDisplayDigit)
         vShiftOut(segment_digit[bDisplayDigit] | segment_data[bDisplayValues[bDisplayDigit]] & ~(1 << 15));
     else
@@ -132,4 +195,114 @@ void vDisplayUpdate() {
 
     bDisplayDigit++;
     if(bDisplayDigit > 3) bDisplayDigit = 0;
+}
+
+
+void vCheckButtons() {
+    // check switch 1
+    if(pinSW1 == 0) {
+        if(bSW1_Timer <= BUTTON_LONG) {
+            if(bSW1_Timer == BUTTON_SHORT) {
+                // event: short press
+            }
+            if(bSW1_Timer == BUTTON_LONG) {
+                // event: long press
+                // enable/disable compare
+                if(bAdcEnableCompare) bAdcEnableCompare = 0;
+                else bAdcEnableCompare = 1;
+            }
+            bSW1_Timer++;
+        } 
+    } else {
+        if((bSW1_Timer >= BUTTON_SHORT) && (bSW1_Timer <= BUTTON_LONG)) {
+            // event: released from short
+            // switch between states
+            if(bDisplayState == StateCompare) bDisplayState = StateSettings;
+            else if(bDisplayState == StateSettings) bDisplayState = StateCompare;
+        }
+        if(bSW1_Timer > BUTTON_LONG) {
+            // event: released from long
+        }
+        bSW1_Timer = 0;
+    }
+
+    // check switch 2
+    if(pinSW2 == 0) {
+        if(bSW2_Timer <= BUTTON_LONG) {
+            if(bSW2_Timer == BUTTON_SHORT) {
+                // event: short press
+            }
+            if(bSW2_Timer == BUTTON_LONG) {
+                // event: long press
+                bButtonUpEnable = 1;
+            }
+            bSW2_Timer++;
+        } 
+    } else {
+        if((bSW2_Timer >= BUTTON_SHORT) && (bSW2_Timer <= BUTTON_LONG)) {
+            // event: released from short
+            if(wAdcSetpoint < 3300) wAdcSetpoint += 100;
+            else wAdcSetpoint = 3300;
+        }
+        if(bSW2_Timer > BUTTON_LONG) {
+            // event: released from long
+            bButtonUpEnable = 0;
+        }
+        bSW2_Timer = 0;
+    }
+
+    // check switch 3
+    if(pinSW3 == 0) {
+        if(bSW3_Timer <= BUTTON_LONG) {
+            if(bSW3_Timer == BUTTON_SHORT) {
+                // event: short press
+            }
+            if(bSW3_Timer == BUTTON_LONG) {
+                // event: long press
+                bButtonDownEnable = 1;
+            }
+            bSW3_Timer++;
+        } 
+    } else {
+        if((bSW3_Timer >= BUTTON_SHORT) && (bSW3_Timer <= BUTTON_LONG)) {
+            // event: released from short
+            if(wAdcSetpoint > 0) wAdcSetpoint -= 100;
+            else wAdcSetpoint = 0;
+        }
+        if(bSW3_Timer > BUTTON_LONG) {
+            // event: released from long
+            bButtonDownEnable = 0;
+        }
+        bSW3_Timer = 0;
+    }
+
+
+    // check button counters
+    if(bButtonUpEnable) {
+        if(bButtonUpCounter == 0) {
+            // reset counter
+            bButtonUpCounter = 40;
+            // count up
+            if(wAdcSetpoint < 3300) wAdcSetpoint += 100;
+            else wAdcSetpoint = 3300;
+        } else {
+            bButtonUpCounter--;
+        }
+    } else {
+        bButtonUpCounter = 0;
+    }
+
+    if(bButtonDownEnable) {
+        if(bButtonDownCounter == 0) {
+            // reset counter
+            bButtonDownCounter = 40;
+            // count down
+            if(wAdcSetpoint > 0) wAdcSetpoint -= 100;
+            else wAdcSetpoint = 0;
+        } else {
+            bButtonDownCounter--;
+        }
+    } else {
+        bButtonDownCounter = 0;
+    }
 }
